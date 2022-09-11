49d9db49bd6a258472da31faf07e38f35d884bef
FarukOfCulture <ecelfaruk05@gmail.com>
2022-09-11 23:59:49 +0300

    commit

---
+++ Rosth/src/main.rs
@@ -0,0 +1,67 @@
+use std::fs;
+
+fn op_pop(stack: &mut Vec<u32>, op: &str, c: u32) -> u32 {
+    stack.pop().expect(&format!(
+        "`{op}` operator can not be usen while there is less then {c} item in stack",
+    ))
+}
+
+fn main() {
+    let file = fs::read_to_string(std::env::args().nth(1).expect("File path must be provided"))
+        .expect("Reading file failed");
+    let program = file.split([' ', '\t', '\n']).collect::<Vec<_>>();
+    let mut i = 0;
+    let mut stack: Vec<u32> = Vec::new();
+    while i < program.len() {
+        let op = program[i];
+        // println!("{op:?}");
+        match op {
+            "" => {}
+            "+" => {
+                let a = op_pop(&mut stack, op, 2);
+                let b = op_pop(&mut stack, op, 2);
+                stack.push(a + b)
+            }
+            "-" => {
+                let a = op_pop(&mut stack, op, 2);
+                let b = op_pop(&mut stack, op, 2);
+                stack.push(a - b)
+            }
+            "." => {
+                println!("{}", op_pop(&mut stack, op, 1))
+            }
+            "=" => {
+                let a = op_pop(&mut stack, op, 2);
+                let b = op_pop(&mut stack, op, 2);
+                stack.push((a == b) as u32)
+            }
+            "!=" => {
+                let a = op_pop(&mut stack, op, 2);
+                let b = op_pop(&mut stack, op, 2);
+                stack.push((a != b) as u32)
+            }
+            "<" => {
+                let a = op_pop(&mut stack, op, 2);
+                let b = op_pop(&mut stack, op, 2);
+                stack.push((a < b) as u32)
+            }
+            ">" => {
+                let a = op_pop(&mut stack, op, 2);
+                let b = op_pop(&mut stack, op, 2);
+                stack.push((a > b) as u32)
+            }
+            "<=" => {
+                let a = op_pop(&mut stack, op, 2);
+                let b = op_pop(&mut stack, op, 2);
+                stack.push((a <= b) as u32)
+            }
+            ">=" => {
+                let a = op_pop(&mut stack, op, 2);
+                let b = op_pop(&mut stack, op, 2);
+                stack.push((a >= b) as u32)
+            }
+            _ => stack.push(op.parse().expect(&format!("`{op}` is unknown operator"))),
+        }
+        i += 1;
+    }
+}
